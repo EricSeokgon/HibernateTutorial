a0a38ba31acdf445ba7a95f13f0cb8f4acfdb5d6
hadesyi <hadesleeq@gmail.com>
2016-01-19 10:58:39 +0900

    Honey class.

---
+++ src/main/java/JPA_example/Honey.java
@@ -0,0 +1,70 @@
+package JPA_example;
+
+import java.io.Serializable;
+import java.util.HashSet;
+
+/**
+ * Project: HibernateTutorial
+ * FileName: Honey
+ * Date: 2016-01-19
+ * Time: 오전 10:55
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Honey implements Serializable {
+    private Integer id;
+    private String name;
+    private String taste;
+    private set<Bee> bees = new HashSet<Bee>();
+
+    public Honey() {
+    }
+
+    public Honey(String name, String taste) {
+        this.name = name;
+        this.taste = taste;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public set<Bee> getBees() {
+        return bees;
+    }
+
+    public void setBees(set<Bee> bees) {
+        this.bees = bees;
+    }
+
+    @Override
+    public String toString() {
+        return "Honey{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", taste='" + taste + '\'' +
+                ", bees=" + bees +
+                '}';
+    }
+}
