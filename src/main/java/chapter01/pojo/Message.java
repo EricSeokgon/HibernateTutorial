c69da861f5d8d4f1d4651eae662565ff78c24c6f
hadesyi <hadesleeq@gmail.com>
2016-01-19 10:21:21 +0900

    The POJO Used in this Chapter’s Examples

---
+++ src/main/java/chapter01/pojo/Message.java
@@ -0,0 +1,29 @@
+package chapter01.pojo;
+
+/**
+ * Project: HibernateTutorial
+ * FileName: Message
+ * Date: 2016-01-19
+ * Time: 오전 10:20
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Message {
+    String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
